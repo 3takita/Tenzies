1347128aab8dfdc5c0ca11a7696405cd7478368d
3takita <98113361+3takita@users.noreply.github.com>
2022-03-19 18:12:52 -0700

    Update1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import "./styles.css";
 import React from "react";
+import Die from "./components/Die";
 
 export default function App() {
+  /**
+   * Challenge:
+   *
+   * Create state to hold our array of numbers. (Initialize
+   * the state by calling our `allNewDice` function so it
+   * loads all new dice as soon as the app loads)
+   *
+   * Map over the state numbers array to generate our array
+   * of Die elements and render those in place of our
+   * manually-written 10 Die elements.
+   */
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const max = 6;
+    const min = 1;
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push(Math.floor(Math.random() * (max - min) + min));
+    }
+    return arr;
+  }
+
+  const diceElement = dice.map((item) => {
+    return <Die value={item} />;
+  });
+
+  function roll() {
+    setDice(allNewDice);
+  }
+
   return (
-    <main className="outer">
-      <div className="inner"></div>
+    <main className="frame">
+      <div className="dice-container">{diceElement}</div>
+      <button className="button" onClick={roll}>
+        Roll
+      </button>
     </main>
   );
 }
---
+++ src/Instructions.md
@@ -0,0 +1,16 @@
+/_
+Write a function (allNewDice) that returns an array of 10 numbers between 1 and 6 inclusive.l Log the array of numbers to the console for now.
+_/
+
+/\*\*
+
+- Challenge:
+-
+- Create state to hold our array of numbers. (Initialize
+- the state by calling our `allNewDice` function so it
+- loads all new dice as soon as the app loads)
+-
+- Map over the state numbers array to generate our array
+- of Die elements and render those in place of our
+- manually-written 10 Die elements.
+  \*/
